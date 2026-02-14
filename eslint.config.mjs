97287f56ac01d6caf71794af6525059edfdec1f2
leiswatch <kowalrox@gmail.com>
2026-02-14 17:01:21 +0100

    batman

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,31 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+    ...nextVitals,
+    ...nextTs,
+    eslintPluginPrettierRecommended,
+    eslintConfigPrettier,
+    {
+        rules: {
+            "sort-keys": [
+                "error",
+                "asc",
+                { caseSensitive: true, minKeys: 2, natural: false },
+            ],
+        },
+    },
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+    ]),
 ]);
 
 export default eslintConfig;
